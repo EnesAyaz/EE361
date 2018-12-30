13fa55edd227101a5b51b0604e57b50ed9904db3
Enes <enes.ayaz.1996@gmail.com>
2018-12-30 16:25:44 +0300

    Final Update

--- HW 4/Homework 4/Final/Solution.m
+++ HW 4/Homework 4/Final/Solution.m
@@ -4,32 +4,6 @@
 %% NAME: _TEMPLATE_
 %% STUDENT NUMBER: 123456
 
-%%
-% Change your .m file name to the following:
-% name_surname_ID_hw4.m
-
-% Most of the operations are already given to you for simplicity.
-% Uncomment the already given solutions to use them, i.e, Delete the "%" in
-% front of such lines.
-
-% Use "fprintf" function to display results
-% Do not display the results of long arrays!!!
-
-% Please add axis names, legends, titles etc. in all your plots
-
-% Use the already defined variable names whenever possible
-
-% Show the unit for each variable
-
-% Examine the whole template before you start
-
-% Delete the hints, guidelines etc. given in this template when you prepare
-% your solution
-
-% Note that:
-% Indexes in MATLAB start at 1, not 0
-% Elementwise operations:
-% http://www.mathworks.com/help/matlab/matlab_prog/array-vs-matrix-operations.html
 
 %% Q1
 
@@ -178,12 +152,5 @@ fprintf(' Average torque is zero' );
 fprintf('The average torque of the releuctance motor is not zero only if mechanical\n ')
 fprintf('speed is is equal to electrical angular frequency and initial poisition is not fold of pi.\n')
 
-%% After you finished
 
-% RUN THE FOLLOWING COMMAND FROM MATLAB TERMINAL (COMMAND WINDOW). DO NOT
-% LEAVE IT IN YOUR .M FILE !!!
-
-% publish('name_surname_ID_hw4.m','pdf')
-
-% A report will be generated in pdf format. ONLY UPLOAD THIS PDF FILE !!!
 
